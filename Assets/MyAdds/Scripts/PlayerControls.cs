de41757d53bb3d38731a2a57c672eb7a8dbfa4cf
William Guay <WillGuay2007@hotmail.com>
2025-11-17 13:03:55 -0500

    shoot on press

--- Assets/MyAdds/Scripts/PlayerControls.cs
+++ Assets/MyAdds/Scripts/PlayerControls.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 //Script fait par moi
 public class PlayerControls : MonoBehaviour
 {
-
+    [SerializeField] private InputActionAsset m_InputActionAsset;
+    private InputAction m_Shoot;
     [SerializeField] private Transform Gun;
     private Animator GunAnimator;
     void Start()
     {
         GunAnimator = Gun.GetComponent<Animator>();
+        m_Shoot = m_InputActionAsset.FindAction("Shoot");
     }
 
     void Update()
     {
-        GunAnimator.SetTrigger("Fire");
+        if (m_Shoot.WasPressedThisFrame())
+        {
+            GunAnimator.SetTrigger("Fire");
+        }
     }
+
+ 
 
     public void Shoot()
     {
